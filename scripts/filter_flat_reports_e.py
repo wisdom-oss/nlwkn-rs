57b2c6812716c089bbb1ad88f28542636cb49f75
Tim Hesse <3590829+cptpiepmatz@users.noreply.github.com>
2023-08-30 17:37:05 +0200

    added a small python script to filter reports.json

---
+++ scripts/filter_flat_reports_e.py
@@ -0,0 +1,24 @@
+# This script is intended to filter all reports so that they only contain water
+# right usage locations where the legal department is 'E'.
+# Then it also trims it down to remove obsolete columns.
+
+import pandas as pd
+import os
+
+data_dir = os.path.dirname(__file__) + "/../data"
+
+# Load CSV file into a DataFrame
+df = pd.read_csv(data_dir + "/reports.csv", sep=";", dtype=str)
+
+# Filter the DataFrame to keep only rows where 'Abteilungskürzel' or 'legal
+# department abbreviation' is 'E'
+filtered_df = df[
+    (df["Abteilungskürzel"] == "E") |
+    (df["legal department abbreviation"] == "E")
+]
+
+# Remove columns with all values empty or NaN
+cleaned_df = filtered_df.dropna(axis=1, how="all")
+
+# Save the cleaned DataFrame to a new CSV file
+cleaned_df.to_csv(data_dir + "/reports_e.csv", sep=";", index=False)
